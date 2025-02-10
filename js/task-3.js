7fc0c65956ce70b71f8ecc95f505730ea687b113
Oleksandr_Boichuk <boichuk315@gmai.com>
2025-02-10 19:42:34 +0100

    edit

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,22 @@
-function filterArray(numbers, value) {
-  const filteredNumbers = [];
-  
-  for (let number of numbers) {
-    if (number > value) {
-      filteredNumbers.push(number);
-    }
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.playTime += hours;
   }
-  
-  return filteredNumbers;
-}
+};
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+// Перевірка
+console.log(profile.getInfo());
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); 
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); 
